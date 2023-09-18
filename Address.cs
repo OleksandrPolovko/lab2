644c9b6ae5a23ef0b16a601f183cd46bfd80e65c
OleksandrPolovko <O.Polovko_FIT_12_22_B_d@knute.edu.ua>
2023-09-18 22:25:20 +0300

    lab2

---
+++ Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Address
+    {
+        private int index;
+        private string country;
+        private string city;
+        private string street;
+        private string house;
+        private int apartment;
+
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+        public string Country { get => country; set => country = value; }
+        public string City { get => city; set => city = value; }
+        public string Street { get => street; set => street = value; }
+        public string House { get => house; set => house = value; }
+        public int Apartment { get => apartment; set => apartment = value; }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            //Створити екземпляр класу Address.
+            Address addr = new Address();
+
+            //У поля екземпляра записати інформацію про поштову адресу. 
+            addr.Index = 07442;
+            addr.Country = "Ukraine";
+            addr.City = "Kyiv";
+            addr.Street = "Parkhomenko";
+            addr.House = "23b";
+            addr.Apartment = 8;
+
+            //Виведіть на екран значення полів, що описують адресу.
+            Console.Out.WriteLine("Index: " + addr.Index);
+            Console.Out.WriteLine("Country: " + addr.Country);
+            Console.Out.WriteLine("City: " + addr.City);
+            Console.Out.WriteLine("Street: " + addr.Street);
+            Console.Out.WriteLine("House: " + addr.House);
+            Console.Out.WriteLine("Apartment: " + addr.Apartment);
+        }
+        static void Task2()
+        {
+            //Створити екземпляр класу Address.
+            Address addr = new Address();
+
+            //У поля екземпляра записати інформацію про поштову адресу. 
+            addr.Index = 07442;
+            addr.Country = "Ukraine";
+            addr.City = "Kyiv";
+            addr.Street = "Parkhomenko";
+            addr.House = "23b";
+            addr.Apartment = 8;
+
+            //Виведіть на екран значення полів, що описують адресу.
+            Console.Out.WriteLine("Index: " + addr.Index);
+            Console.Out.WriteLine("Country: " + addr.Country);
+            Console.Out.WriteLine("City: " + addr.City);
+            Console.Out.WriteLine("Street: " + addr.Street);
+            Console.Out.WriteLine("House: " + addr.House);
+            Console.Out.WriteLine("Apartment: " + addr.Apartment);
+        }
+
+        static void Main(string[] args)
+        {
+            Task1();
+        }
+    }
+}
